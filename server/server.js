9c00d604abfab1a70c9a58db149220e199538777
Mahalakshmi <mahakgm0827@gmail.com>
2024-11-08 15:07:04 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const dbConn = require("./config/db");
-// const Projects = require("./routes/projectRoute");
+
 const Users = require("./routes/userRoute");
 const Blogs = require("./routes/blogRoute");
 app.use(express.json());
 
 app.use(cors());
 const port = process.env.PORT || 7778;
-// app.use("/projects", Projects);
+
 app.use("/users", Users);
 app.use("/blogs", Blogs);
 
